d132a9b99dbfaccaf77dc2e9c735ed8467c2b725
GRUPOGMV\bbll <blopez@gmv.com>
2025-07-27 17:01:38 +0200

    add e2e test

---
+++ src/test/java/es/ecommerce/e2e/test/PriceE2ETest.java
@@ -0,0 +1,68 @@
+package es.ecommerce.e2e.test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.jdbc.SqlConfig.ErrorMode;
+
+import io.restassured.RestAssured;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Sql(scripts = { "/data_for_tests.sql" }, config = @SqlConfig(errorMode = ErrorMode.CONTINUE_ON_ERROR))
+@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
+public class PriceE2ETest {
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = this.port;
+	}
+
+	// Test 1: 14 de junio 10:00
+	@Test
+	void test1_shouldReturn_35_50() {
+		given().queryParam("suppliedDate", "2020-06-14T10:00:00").queryParam("productId", 35455).queryParam("brandId", 1).when()
+				.get("/prices").then().statusCode(200).body("price", equalTo(35.5f));
+	}
+
+	// Test 2: 14 de junio 16:00
+	@Test
+	void test2_shouldReturn_25_45() {
+		given().queryParam("suppliedDate", "2020-06-14T16:00:00").queryParam("productId", 35455).queryParam("brandId", 1).when()
+				.get("/prices").then().statusCode(200).body("price", equalTo(25.45f));
+	}
+
+	// Test 3: 14 de junio 21:00
+	@Test
+	void test3_shouldReturn_35_50() {
+		given().queryParam("suppliedDate", "2020-06-14T21:00:00").queryParam("productId", 35455).queryParam("brandId", 1).when()
+				.get("/prices").then().statusCode(200).body("price", equalTo(35.5f));
+	}
+
+	// Test 4: 15 de junio 10:00
+	@Test
+	void test4_shouldReturn_30_50() {
+		given().queryParam("suppliedDate", "2020-06-15T10:00:00").queryParam("productId", 35455).queryParam("brandId", 1).when()
+				.get("/prices").then().statusCode(200).body("price", equalTo(30.5f));
+	}
+
+	// Test 5: 16 de junio 21:00
+	@Test
+	void test5_shouldReturn_38_95() {
+		given().queryParam("suppliedDate", "2020-06-16T21:00:00").queryParam("productId", 35455).queryParam("brandId", 1).when()
+				.get("/prices").then().statusCode(200).body("price", equalTo(38.95f));
+	}
+}
